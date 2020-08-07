291f1265370ee72f4f03f0ce2e114a1fec9d1821
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-07 17:55:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,7 +59,8 @@ class TicTacToe
     end
  #Your #won? method should return false/nil if there is no win combination present in the board and return the winning combination indexes as an array if there is a win. Use your WIN_COMBINATIONS constant in this method.
   def won?
-
+WIN_COMBINATIONS.detect do |win_combo|
+  
   end
     WIN_COMBINATIONS = [
     [0,1,2],
