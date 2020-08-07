a7ca8b19e9666559525f610cb90f0148b04f32df
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-07 16:47:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,6 +2,16 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
+  def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  def input_to_index(index)
+    input_to_index-1
+  end
     WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
@@ -11,11 +21,6 @@ class TicTacToe
     [2,5,8],
     [0,4,8],
     [2,6,4],]
-  def display_board
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-  puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-  puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
+
+  
 end
