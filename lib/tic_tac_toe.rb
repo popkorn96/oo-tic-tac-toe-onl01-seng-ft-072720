65947829a415789eb8eac18da598f9663e0c8f60
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-07 16:44:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,8 +11,11 @@ class TicTacToe
     [2,5,8],
     [0,4,8],
     [2,6,4],]
-  def self.display_board
-    @board = board
-    board
+  def display_board
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
 end
