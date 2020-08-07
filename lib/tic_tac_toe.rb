9bc2fc87f221d1c886ec240a2d0e69da07bfb447
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-07 16:58:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,7 +9,7 @@ class TicTacToe
   puts "-----------"
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  def input_to_index(index)
+  def input_to_index(input)
     input.to_i - 1
   end
   def move(index,token = X)
