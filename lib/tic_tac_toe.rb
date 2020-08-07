42fa0888a7108e4d115230cb388020352bfbf3e4
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-07 17:40:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,7 +49,7 @@ class TicTacToe
   
   def turn
     puts "Please input a number between 1 and 9"
-    input = gets.strip
+    input = gets
     index = input_to_index
     char = current_player
     if valid_move?(index)
