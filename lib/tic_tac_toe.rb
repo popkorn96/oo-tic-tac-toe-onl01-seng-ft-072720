8dc9a4b5b4f390e2689c278ee5dc8d44f5d02600
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-07 17:11:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,7 +19,7 @@ class TicTacToe
     ((@board[index] == "X") || (@board[index] == "O"))
   end
   def valid_move?
-    index.between?(0..8) && position_taken?(index)
+    index.between?(0,8) && !position_taken?(index)
   end
     
     WIN_COMBINATIONS = [
