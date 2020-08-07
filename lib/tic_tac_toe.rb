72dd460c4df4e373c321edd6ddd01be5d9ee5c10
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-07 18:26:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -82,7 +82,7 @@ def over?
   (draw?) || (won?) || (full?)
 end
 
-def winner?
+def winner
   WIN_COMBINATIONS.detect do |win_combo|
   if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
     return "X"
